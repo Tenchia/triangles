a5e72aa11d3c7967a397775d68388d24721d7f40
Chicha <Chicha@DESKTOP-I94U09R>
2021-12-20 11:54:01 +0200

    Добавьте файлы проекта.

---
+++ triangles/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace triangles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class triangle
+    {
+        public double A, B, C, a, b, c, p, r, h, S, P;
+
+        public void perimeter()
+        {
+            P = A + B + C;
+        }
+        public void semiPerimeter()
+        {
+            p = P / 2;
+        }
+        public void semiPerimeterSquare()
+        {
+            S = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+
+        public void innerRadiusSquare()
+        {
+            r = Math.Sqrt((p - A)*(p - B)*(p - C) / 2);
+            S = r * (P / 2);
+        }
+
+
+    }
+}
